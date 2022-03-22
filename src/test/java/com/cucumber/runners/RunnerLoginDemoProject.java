6539648044d1c7e66c10bc2aea7107d61502a92d
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-03-22 06:37:53 -0500

    initial code

---
+++ src/test/java/com/cucumber/runners/RunnerLoginDemoProject.java
@@ -0,0 +1,16 @@
+package com.cucumber.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/java/resources/features/loginDemoProject.feature"
+        , glue = "stepDefinition"
+        , monochrome = false
+        , dryRun = false
+        , plugin = {"pretty", "html:target/HTMLReports/report.html"}
+        , tags = "" // Example => "{@smoketest}"  // (and) and (or) is aplicable with this feature
+)
+public class RunnerLoginDemoProject extends AbstractTestNGCucumberTests {
+
+}
---
+++ src/test/java/com/cucumber/runners/RunnerSearch.java
@@ -0,0 +1,15 @@
+package com.cucumber.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/java/resources/features/search.feature"
+        , glue = "stepDefinition"
+        , monochrome = false
+        , plugin = {"pretty", "html:target/HTMLReports/report.html"}
+        , tags = "" // Example => @smoketest
+)
+public class RunnerSearch extends AbstractTestNGCucumberTests {
+
+}
